ebbd1318e67d69b4fb4f5eb05f3467be13d83902
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2023-12-30 16:36:28 +1300

    refactor: stuff for ex2.1 and 2.2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,15 +1,15 @@
 import Part from './Part'
 
 const Content = (props) => {
-    const partsArray = props.parts
+  const partsArray = props.parts
 
-    return (
-        <div>
-            <Part part={partsArray[0]} />
-            <Part part={partsArray[1]} />
-            <Part part={partsArray[2]} />
-        </div>
-    )
+  return (
+    <div>
+      <Part part={partsArray[0]} />
+      <Part part={partsArray[1]} />
+      <Part part={partsArray[2]} />
+    </div>
+  )
 }
 
 export default Content
--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,14 +1,14 @@
 const Total = (props) => {
-    const partsArray = props.parts
+  const partsArray = props.parts
 
-    // TODO: surely smarter way to do this using loop thing
-    const total = partsArray[0].exercises + partsArray[1].exercises + partsArray[2].exercises
+  // TODO: surely smarter way to do this using loop thing
+  const total = partsArray[0].exercises + partsArray[1].exercises + partsArray[2].exercises
 
-    return (
-        <div>
-            <p>Number of exercises {total}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p><b>Total of {total} exercises</b></p>
+    </div>
+  )
 }
 
 export default Total
