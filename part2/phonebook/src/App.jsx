1c61e3e05b1371fa23e8953a42cfcef18f76be24
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-01-04 20:02:21 +1300

    feat: create phonebook and do 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = event => {
+    event.preventDefault()
+
+    const newPerson = {
+      name: newName
+    }
+
+    console.log(persons.concat(newPerson))
+
+    setPersons(persons.concat(newPerson))
+  }
+
+  const handlePersonChange = event => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <li key={person.name}>{person.name}</li>)}
+    </div>
+  )
+}
+
+export default App
