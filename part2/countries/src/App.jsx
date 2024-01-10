4cf73fa5d95e6b21a922e46af46d1831506599c5
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-01-10 17:44:23 +1300

    feat: added input field for countries and blindly fumbling

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import CountryInput from './components/CountryInput'
+
+/*
+  CountryInput - find countries <Input
+    shall include the onChange thing and be bound to a state
+
+  CountryList component - needs a way to disappear when only one country
+
+  CountryData component - Shall list all the country data given the thing 
+*/
 
 const App = () => {
+  const [search, setSearch] = useState('')
+
+  // useEffect(() => {
+  //   axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+  // })
+
+  const changeSearch = (event) => {
+    setSearch(event.target.value)
+  }
 
   return (
     <div>
-      Hello World!!!
+      <CountryInput value={search} onChange={changeSearch} />
     </div>
   )
 }
---
+++ part2/countries/src/components/CountryInput.jsx
@@ -0,0 +1,10 @@
+const CountryInput = ({ value, onChange }) => {
+
+  return (
+    <div>
+      find countries <input value={value} onChange={onChange} />
+    </div>
+  )
+}
+
+export default CountryInput
