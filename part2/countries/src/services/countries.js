e64938cd37693d50537a323ae97a1e55b73a3e94
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-01-11 02:25:56 +1300

    refactor: add baseUrl for countryService readability

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+
 const getAll = () => {
-  const request = axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+  const request = axios.get(`${baseUrl}/all`)
   return request.then(response => response.data)
 }
 
 const getCountry = (country) => {
-  const request = axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+  const request = axios.get(`${baseUrl}/name/${country}`)
   return request.then(response => response.data)
 }
 
