32b9ab3ad969b99c86c918c6c8ee8ae226af79cc
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-01-15 15:38:21 +1300

    feat: can delete individual people now nice (3.4)

--- part 3/index.js
+++ part 3/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const data = [
+let people = [
   {
     "id": 1,
     "name": "Arto Hellas",
@@ -26,16 +26,23 @@ const data = [
 ]
 
 app.get('/', (request, response) => {
-  response.json(data)
+  response.json(people)
 })
 
 app.get('/:id', (request, response) => {
   const id = Number(request.params.id)
-  response.json(data.find(entry => entry.id === id))
+  response.json(people.find(entry => entry.id === id))
+})
+
+app.delete('/:id', (request, response) => {
+  const id = Number(request.params.id)
+  people = people.filter(entry => entry.id !== id)
+
+  response.status(204).end()
 })
 
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${data.length} people</p>
+  response.send(`<p>Phonebook has info for ${people.length} people</p>
                  <p>${new Date()}</p>`)
 })
 
