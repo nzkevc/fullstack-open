d5da832bee033d6f485d9e2b337a22b5b92ad454
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-01-15 14:35:25 +1300

    feat: more data to CountryData component nice

--- part2/countries/src/components/CountryData.jsx
+++ part2/countries/src/components/CountryData.jsx
@@ -3,7 +3,14 @@ const CountryData = ({ country }) => {
   if (country) {
     return (
       <div>
-        {country.name.official}
+        <h1>{country.name.common}</h1>
+        <p>capital {country.capital}</p>
+        <p>area {country.area}</p>
+
+        <h3>Languages</h3>
+        {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+
+        <img src={country.flags.png} alt={country.flags.alt} />
       </div>
     )
   }
