cb7424a2b4769328e458b717374557d01c30f563
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-01-19 12:15:03 +1300

    feat: database integration complete (3.15-3.18) except error handling

--- part3/index.js
+++ part3/index.js
@@ -8,37 +8,10 @@ const app = express()
 morgan.token('json-post', (request) => {
   return JSON.stringify(request.body)
 })
-
-app.use(express.json())
-app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json-post'))
 
-// let people = [
-//   {
-//     "id": 1,
-//     "name": "Arto Hellas",
-//     "number": "040-123456"
-//   },
-//   {
-//     "id": 2,
-//     "name": "Ada Lovelace",
-//     "number": "39-44-5323523"
-//   },
-//   {
-//     "id": 3,
-//     "name": "Dan Abramov",
-//     "number": "12-43-234345"
-//   },
-//   {
-//     "id": 4,
-//     "name": "Mary Poppendieck",
-//     "number": "39-23-6423122"
-//   }
-// ]
-
-const generateId = () => {
-  return Math.round(Math.random() * 10000)
-}
+app.use(express.static('dist'))
+app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(people => {
@@ -46,19 +19,24 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-// TODO: error handling for when person doesn't exist?
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-// TODO: update for mongodb support
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  people = people.filter(entry => entry.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // TODO: extract error handling question mark?
@@ -89,11 +67,46 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-// TODO: update for mongodb support
-app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${people.length} people</p>
-                 <p>${new Date()}</p>`)
+app.put('api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
+
+app.get('/info', (request, response) => {
+  Person.find({})
+    .then(people => {
+      response.send(
+        `<p>Phonebook has info for ${Object.keys(people).length} people</p>
+         <p>${new Date()}</p>`
+      )
+    })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+// TODO: should handle wrong ids for put AND get, name already existing?, deletion error?
+// CastError - malformatted id
+const errorHandler = (error, request, response, next) => {
+  if (error === 'CastError') {
+    response.status(400).send({ error: 'malformatted id' })
+  }
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
