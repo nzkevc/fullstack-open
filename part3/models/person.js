f23ada50e0afc227828efe2b961473d3baaefe05
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-01-19 15:04:56 +1300

    feat: phone number format validation

--- part3/models/person.js
+++ part3/models/person.js
@@ -24,7 +24,13 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     minLength: 8,
-    required: true
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /\d{2,3}-\d+/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
   }
 })
 
